f388bcb60767d56b603b1952afa061e33320e48c
ruvick <rudikov.web@ya.ru>
2021-02-02 19:15:41 +0300

    Шапка + Слайдер

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,22 +1,3 @@
-var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
-if (isMobile.any()) { }
-
-if (location.hash) {
-	var hsh = location.hash.replace('#', '');
-	if ($('.popup-' + hsh).length > 0) {
-		popupOpen(hsh);
-	} else if ($('div.' + hsh).length > 0) {
-		$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
-	}
-}
-$('.wrapper').addClass('loaded');
-
-var act = "click";
-if (isMobile.iOS()) {
-	var act = "touchstart";
-}
-
-
 
 //BURGER
 let iconMenu = document.querySelector(".icon-menu"); 
@@ -73,6 +54,40 @@ jQuery('.header__form button').click(function(e){
         });
  
     });
+
+
+// Slider
+	$('.info-sl__slider').slick( {
+		arrows: false,
+		dots: true,
+  	infinite: true,
+  	speed: 1000,
+  	slidesToShow: 1,
+  	autoplay: true,
+  	autoplaySpeed: 1800,
+  	adaptiveHeight: true
+	});
+
+
+
+var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+if (isMobile.any()) { }
+
+if (location.hash) {
+	var hsh = location.hash.replace('#', '');
+	if ($('.popup-' + hsh).length > 0) {
+		popupOpen(hsh);
+	} else if ($('div.' + hsh).length > 0) {
+		$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
+	}
+}
+$('.wrapper').addClass('loaded');
+
+var act = "click";
+if (isMobile.iOS()) {
+	var act = "touchstart";
+}
+
 
 
 
